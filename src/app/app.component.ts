df3d5070c0d7e863ef0c3b7b7720e1989f3fc3d4
Alvaro <agar2059@gmail.com>
2025-05-22 20:38:02 +0200

    Delete usage of CallToAction into the app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { CalltoactionComponent } from "./components/calltoaction/calltoaction.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, CalltoactionComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
