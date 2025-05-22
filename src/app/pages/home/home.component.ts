006cd0aea871c7b6fc4a0680d20801a6dac26cd5
Alvaro <agar2059@gmail.com>
2025-05-22 20:11:12 +0200

    Add Component CallToAction into Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CalltoactionComponent } from '../../components/calltoaction/calltoaction.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CalltoactionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
