1a181fd189fa0a43ada87c72907269cbd8134ca7
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-05-22 18:57:37 +0200

    componente Call to action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CalltoactionComponent } from "./components/calltoaction/calltoaction.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CalltoactionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/calltoaction/calltoaction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calltoaction',
+  imports: [],
+  templateUrl: './calltoaction.component.html',
+  styleUrl: './calltoaction.component.css'
+})
+export class CalltoactionComponent {
+
+}
