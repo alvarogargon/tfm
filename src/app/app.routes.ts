06f85512ff3e44ee7ba4973d929d1026a17b395d
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-05-24 09:58:59 +0200

    css-btn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "home" },
     { path: "home", component: HomeComponent },
     { path: "login", component: LoginComponent },
     { path: "register", component: RegisterComponent },
-    { path: "**", redirectTo: "home"}
+    { path: "**", component: Error404Component}
 ];
--- src/app/components/calltoaction/calltoaction.component.ts
+++ src/app/components/calltoaction/calltoaction.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-calltoaction',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './calltoaction.component.html',
   styleUrl: './calltoaction.component.css'
 })
