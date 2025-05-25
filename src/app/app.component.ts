aac3eb171f46a5219199a5439d9e4a88c5ad89e1
alvarocrsc <alvarocrsc@gmail.com>
2025-05-26 00:14:45 +0200

    Added Features Component to Home, Installed Montserrat font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import "@fontsource/montserrat"; 
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/200.css";
 
 @Component({
   selector: 'app-root',
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CalltoactionComponent } from '../../components/calltoaction/calltoaction.component';
+import { FeaturesComponent } from "../../components/features/features.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CalltoactionComponent],
+  imports: [CalltoactionComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
