595c574a531afa6130ca838f2df99b6981f2dcca
Álvaro <alvarocrsc@gmail.com>
2025-06-02 14:50:22 +0200

    Updated Styles of Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/200.css";
+import "@fontsource/geist-mono";
 import { Footer2Component } from './shared/footer2/footer2.component';
 
 @Component({
