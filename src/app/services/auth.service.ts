908d99a6dafe6b1dbf9901910bbc620d66c3c10c
Álvaro <alvarocrsc@gmail.com>
2025-06-05 10:03:56 +0200

    Implemented Login Logic, successful Login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUserLogin } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
+
+interface LoginResponse {
+  message: string; 
+  token: string;
+  user: {
+    userId: number;
+    email: string;
+    role: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private endpoint = 'http://localhost:3000/api/auth';
+  private httpClient = inject(HttpClient);
+
+  login(credentials: IUserLogin): Promise<LoginResponse> {
+    return lastValueFrom(this.httpClient.post<LoginResponse>(`${this.endpoint}/login`, credentials));
+  }
+}
