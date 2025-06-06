25aee87b4098b4712152ad12a542ef3aca01a892
Álvaro <alvarocrsc@gmail.com>
2025-06-06 14:27:15 +0200

    Home updated

--- src/app/components/calltoaction/calltoaction.component.ts
+++ src/app/components/calltoaction/calltoaction.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+declare var VANTA: any;
 
 @Component({
   selector: 'app-calltoaction',
@@ -7,6 +9,32 @@ import { RouterLink } from '@angular/router';
   templateUrl: './calltoaction.component.html',
   styleUrl: './calltoaction.component.css'
 })
-export class CalltoactionComponent {
+export class CalltoactionComponent implements AfterViewInit, OnDestroy {
+
+  private vantaEffect: any;
+
+  ngAfterViewInit() {
+    this.vantaEffect = VANTA.FOG({
+      el: '.vanta-bg-home',
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      highlightColor: 0xdbedff,
+      midtoneColor: 0x3434dc,
+      lowlightColor: 0x4646a2,
+      baseColor: 0xffffff,
+      blurFactor: 0.20,
+      speed: 0.10,
+      zoom: 0.10
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy();
+    }
+  }
 
 }
