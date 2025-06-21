b23c55560e4b24b4f68a5127d5574878d384fff6
alvarocrsc <alvarocrsc@gmail.com>
2025-06-21 17:52:39 +0200

    Changed handling of Profile Pictures with Files, Implemented Updates

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { EditProfileComponent } from "./edit-profile/edit-profile.component";
 import { IUser } from '../../interfaces/iuser.interface';
 import { UserService } from '../../services/user.service';
@@ -13,6 +13,32 @@ export class ProfileComponent {
   showEditForm = false;
   user?: IUser;
   userService = inject(UserService);
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
+  onAvatarClick() {
+    this.fileInput.nativeElement.click();
+  }
+
+  async onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+
+      await this.uploadProfileImage(file);
+    }
+  }
+
+  async uploadProfileImage(file: File) {
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+      const updatedUser = await this.userService.updateProfileImage(formData);
+      this.user = updatedUser;
+    } catch (error) {
+      console.error('Error uploading profile image:', error);
+    }
+  }
 
   async ngOnInit() {
     try {
