331542839d2a5ac25022306045e16420a103bcb4
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-06-28 09:20:19 +0200

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,12 +8,16 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { CalendarPageComponent } from './pages/calendar-page/calendar-page.component';
 import { RoutineDetailsComponent } from './components/routine-details/routine-details.component';
 import { authGuard } from './guards/auth.guard';
+import { TeamListComponent } from './shared/infoFooter/equipo/team-list/team-list.component';
+import { NuestraMisionComponent } from './shared/infoFooter/nuestra-mision/nuestra-mision.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'nuestra-mision', component: NuestraMisionComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'dashboard/calendar', component: CalendarPageComponent, canActivate: [authGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
